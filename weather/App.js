a60e7adc04af423394db1c61d298d02b4e0601d8
shawn-nyquist <shawn.nyquist@gmail.com>
2020-02-21 21:31:33 -0600

    Completed up to page 54

---
+++ weather/App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { StyleSheet, Text, View, Platform, TextInput, KeyboardAvoidingView, ImageBackground } from 'react-native';
+import getImageForWeather from './utils/getImageForWeather';
+import SearchInput from './components/SearchInput';
+
+
+
+
+export default function App() {
+  const location = 'San Francisco';
+
+  return (
+    
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="padding"
+      >
+        <ImageBackground
+          source={getImageForWeather('Clear')}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>
+              {location}
+            </Text>
+            <Text style={[styles.smallText, styles.textStyle]}>
+              Light Cloud
+            </Text>
+            <Text style={[styles.largeText, styles.textStyle]}>
+              24°
+            </Text>
+          
+            <SearchInput placeholder="Search any city" />
+          </View>
+        </ImageBackground> 
+      </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#34495E',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba (0,0,0,0.2)',
+    paddingHorizontal: 20,
+  },
+  textStyle: {
+    textAlign: 'center',
+    ...Platform.select({
+      ios: {
+        fontFamily: 'AvenirNext-Regular',
+      },
+      android: { 
+        fontFamily: 'Roboto',
+      }
+    }),
+    color: 'white',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+  TextInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
+  }
+});
